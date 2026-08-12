f6b51271a10656698e876a52530369971d692e08
Repause Codes <hiteshsahoo18@gmail.com>
2026-08-12 23:30:08 +0530

    Refactor Blynk credentials and log messages
    Updated Blynk credentials and improved detection messages.

--- iot_application.ino
+++ iot_application.ino
@@ -1,22 +1,21 @@
-#define BLYNK_TEMPLATE_ID "TMPL3DzatQEzc"
+#define BLYNK_TEMPLATE_ID "YOUR_TEMPLATE_ID"
 #define BLYNK_TEMPLATE_NAME "Fire Alert System"
-#define BLYNK_AUTH_TOKEN "PIRdViLvuDjaPB37fEPGUKjhfx9UCdYV"
+#define BLYNK_AUTH_TOKEN "YOUR_BLYNK_AUTH_TOKEN"
 
 #include <WiFi.h>
 #include <WiFiClient.h>
 #include <BlynkSimpleEsp32.h>
 
-char ssid[] = "HS2006";
-char pass[] = "S@hoo123454";
+char ssid[] = "YOUR_WIFI_SSID";
+char pass[] = "YOUR_WIFI_PASSWORD";
 
 #define FLAME_SENSOR_PIN 34
 #define BUZZER_PIN 27
 
-bool flamePreviouslyDetected = false;  
+bool flamePreviouslyDetected = false;
 
 void setup() {
   Serial.begin(115200);
-
   pinMode(FLAME_SENSOR_PIN, INPUT);
   pinMode(BUZZER_PIN, OUTPUT);
 
@@ -29,24 +28,18 @@ void loop() {
 
   int flame = digitalRead(FLAME_SENSOR_PIN);
 
-  // --- FLAME DETECTED ---
-  if (flame == LOW && flamePreviouslyDetected == false) {
-    flamePreviouslyDetected = true;  
-
-    Serial.println("🔥 FLAME DETECTED!");
+  if (flame == LOW && !flamePreviouslyDetected) {
+    flamePreviouslyDetected = true;
+    Serial.println("Flame detected");
     tone(BUZZER_PIN, 1000);
-
-    // Trigger Blynk event ONLY when state changes
-    Blynk.logEvent("fire_alert", "FIRE DETECTED! Evacuate Immediately!");
+    Blynk.logEvent("fire_alert", "Fire detected. Evacuate immediately.");
   }
 
-  // --- FLAME GONE ---
-  if (flame == HIGH && flamePreviouslyDetected == true) {
-    flamePreviouslyDetected = false;   
-
-    Serial.println("🔥 Flame cleared.");
+  if (flame == HIGH && flamePreviouslyDetected) {
+    flamePreviouslyDetected = false;
+    Serial.println("Flame cleared");
     noTone(BUZZER_PIN);
   }
 
-  delay(50);  
+  delay(50);
 }
